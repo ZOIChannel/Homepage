ee95b4a753ef02c99dee344de0604ab6e55d7dce
ZOI <zoizoichannel@gmail.com>
2020-02-12 21:33:08 +0900

    2/12_21:33

--- assets/js/server/server.js
+++ assets/js/server/server.js
@@ -26,64 +26,72 @@ function ServerMain(serverIp, serverNumber) {
         "https://api.minetools.eu/ping/" + serverIp,
         null,
         function(data, status) {
-            var description = data.description.split('§');
-            var LENGTH = description.length;
-            for (var i = 0; i < LENGTH; i++) {
-                if (description[i].slice(0, 1) == "0") {
-                    description[i] = "<span style='color:#000000;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "1") {
-                    description[i] = "<span style='color:#0000AA;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "2") {
-                    description[i] = "<span style='color:#00AA00;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "3") {
-                    description[i] = "<span style='color:#00AAAA;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "4") {
-                    description[i] = "<span style='color:#AA0000;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "5") {
-                    description[i] = "<span style='color:#AA00AA;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "6") {
-                    description[i] = "<span style='color:#FFAA00;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "7") {
-                    description[i] = "<span style='color:#AAAAAA;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "8") {
-                    description[i] = "<span style='color:#555555;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "9") {
-                    description[i] = "<span style='color:#5555FF;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "a") {
-                    description[i] = "<span style='color:#55FF55;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "b") {
-                    description[i] = "<span style='color:#55FFFF;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "c") {
-                    description[i] = "<span style='color:#FF5555;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "d") {
-                    description[i] = "<span style='color:#FF55FF;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "e") {
-                    description[i] = "<span style='color:#FFFF55;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "f") {
-                    description[i] = "<span style='color:#FFFFFF;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "l") {
-                    description[i] = "<span style='font-weight:bolder;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "m") {
-                    description[i] = "<span style='text-decoration: line-through;>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "n") {
-                    description[i] = "<span style='text-decoration: underline;>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "o") {
-                    description[i] = "<span style='font-style: italic;'>" + description[i].slice(1);
-                } else if (description[i].slice(0, 1) == "r") {
-                    description[i] = "<span style='color:#aaaaaa; font-style: none;'>" + description[i].slice(1);
+            if (data.description) {
+                console.log("yes");
+                var description = data.description.split('§');
+                var LENGTH = description.length;
+                for (var i = 0; i < LENGTH; i++) {
+                    if (description[i].slice(0, 1) == "0") {
+                        description[i] = "<span style='color:#000000;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "1") {
+                        description[i] = "<span style='color:#0000AA;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "2") {
+                        description[i] = "<span style='color:#00AA00;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "3") {
+                        description[i] = "<span style='color:#00AAAA;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "4") {
+                        description[i] = "<span style='color:#AA0000;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "5") {
+                        description[i] = "<span style='color:#AA00AA;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "6") {
+                        description[i] = "<span style='color:#FFAA00;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "7") {
+                        description[i] = "<span style='color:#AAAAAA;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "8") {
+                        description[i] = "<span style='color:#555555;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "9") {
+                        description[i] = "<span style='color:#5555FF;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "a") {
+                        description[i] = "<span style='color:#55FF55;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "b") {
+                        description[i] = "<span style='color:#55FFFF;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "c") {
+                        description[i] = "<span style='color:#FF5555;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "d") {
+                        description[i] = "<span style='color:#FF55FF;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "e") {
+                        description[i] = "<span style='color:#FFFF55;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "f") {
+                        description[i] = "<span style='color:#FFFFFF;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "l") {
+                        description[i] = "<span style='font-weight:bolder;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "m") {
+                        description[i] = "<span style='text-decoration: line-through;>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "n") {
+                        description[i] = "<span style='text-decoration: underline;>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "o") {
+                        description[i] = "<span style='font-style: italic;'>" + description[i].slice(1);
+                    } else if (description[i].slice(0, 1) == "r") {
+                        description[i] = "<span style='color:#aaaaaa; font-style: none;'>" + description[i].slice(1);
+                    }
                 }
+                console.log("§変換成功!!");
+                data.description = description.join("");
+                console.log("data.description結合成功!!");
+                for (var i = 0; i < LENGTH; i++) {
+                    description = description + "</span>";
+                }
+                console.log("</span>成功!!");
+                getPing_1(data, result1);
+                getPing_2(data, result2);
+                getPing_3(data, result3);
+                getPing_img(data, resultImg, serverIp, serverNumber);
+            } else {
+                getPing_1(data, result1);
+                getPing_2(data, result2);
+                getPing_3(data, result3);
+                getPing_img(data, resultImg, serverIp, serverNumber);
             }
-            console.log("§変換成功!!");
-            data.description = description.join("");
-            console.log("data.description結合成功!!");
-            for (var i = 0; i < LENGTH; i++) {
-                description = description + "</span>";
-            }
-            console.log("</span>成功!!");
-            getPing_1(data, result1);
-            getPing_2(data, result2);
-            getPing_3(data, result3);
-            getPing_img(data, resultImg, serverIp, serverNumber);
         }
     )
 }
@@ -126,7 +134,7 @@ function getPing_3(data, result3) {
     if (data.error == null) {
         line3 = "VERSION : " + data.version.name;
     } else {
-        line3 = "<span style='color:#FFFFFF;'>!!!ERROR!!!</span>";
+        line3 = "<span style='color:#ff0000;'>!!!ERROR!!!</span>";
     }
     result3.innerHTML = line3;
 }
