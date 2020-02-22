ebf27efe8482c336aeedf10e6769ecfbf7e7468f
ZOI <zoizoichannel@gmail.com>
2020-02-22 21:03:07 +0900

    ======= autoweb by macbook air =======

--- assets/js/contact/contact.js
+++ assets/js/contact/contact.js
@@ -1,15 +1,34 @@
 function ContactMeSubmit() {
-    console.log(`コメント(zoizoi.net, "${document.getElementById("ZOIsNameInput").value}"より)`);
-    Email.send({
-        Host: "smtp.gmail.com",
-        Username: "zoichannel",
-        Password: "tori2box",
-        To: "zoi@zoizoi.net",
-        From: "user@zoizoi.net",
-        Subject: `コメント(zoizoi.net, "${document.getElementById("ZOIsNameInput").value}"より)`,
-        Body: `${document.getElementById("ZOIsTextValue").value}\n----ここまで----\n返信先:${document.getElementById("ZOIsEmailInput").value}`
-    }).then(
-        message => alert(message)
-    );
-    console.log("Okay")
+
+    var ZOIsNameInput = document.getElementById("ZOIsNameInput").value;
+    var ZOIsEmailInput = document.getElementById("ZOIsEmailInput").value;
+    var ZOIsTextValue = document.getElementById("ZOIsTextValue").value;
+
+    var to = "zoi@zoizoi.net";
+    var subject = `コメント(zoizoi.net, "${document.getElementById("ZOIsNameInput").value}"より)`;
+    var message = `${document.getElementById("ZOIsTextValue").value}\n\n返信先:${document.getElementById("ZOIsEmailInput").value}`;
+    var headers = "From: user@zoizoi.net";
+    /*
+    console.log(to);
+    console.log(subject);
+    console.log(message);
+    console.log(headers);
+    */
+    if (to && subject && message && headers) {
+        console.log("ok.");
+        $.post(
+                'http://zoichannel.php.xdomain.jp/mail.php',
+                `to=${to}&subject=${subject}&message=${message}&headers=${headers}`
+            )
+            .done(function(data) {
+                // console.log(data.form);
+                alert("送信が完了しました。");
+            })
+    } else {
+        console.log("out.");
+        document.getElementById("ZOIsNameInput").value = ZOIsNameInput;
+        document.getElementById("ZOIsEmailInput").value = ZOIsEmailInput;
+        document.getElementById("ZOIsTextValue").value = ZOIsTextValue;
+        alert("入力していない欄があります。");
+    }
 }
